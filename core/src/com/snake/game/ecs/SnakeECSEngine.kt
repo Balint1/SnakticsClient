fe824dacc4e32e40b4075733ff66d61cab927075
KelianB <kelian.baert@gmail.com>
2020-03-30 13:10:31 +0200

    added ComponentSyncSystem to the ECSEngine constructor

--- core/src/com/snake/game/ecs/SnakeECSEngine.kt
+++ core/src/com/snake/game/ecs/SnakeECSEngine.kt
@@ -4,14 +4,16 @@ import com.badlogic.gdx.Gdx
 import com.snake.game.ecs.component.PositionComponent
 import com.snake.game.ecs.entity.Entity
 import com.snake.game.ecs.system.AnimationSystem
+import com.snake.game.ecs.system.ComponentSyncSystem
 import com.snake.game.ecs.system.RenderingSystem
+import java.util.*
 
 object SnakeECSEngine: ECSEngine(
-        AnimationSystem, RenderingSystem) {
+        ComponentSyncSystem, AnimationSystem, RenderingSystem) {
     init {
 
         // Testing
-        var test = Entity(entityManager);
+        var test = Entity(UUID.randomUUID().toString(), entityManager);
         test.addComponent(PositionComponent());
         Gdx.app.debug("test", "testing update");
         update(30f);
