c12a700549cd6a633ba78705c85c8f8ae8fd43c3
Bartha Bálint <bartha.balint@hotmail.com>
2020-04-25 13:44:02 +0200

    Invisible powerup setup

--- core/src/com/snake/game/ecs/component/SnakeComponent.kt
+++ core/src/com/snake/game/ecs/component/SnakeComponent.kt
@@ -4,10 +4,12 @@ import org.json.JSONObject
 
 class SnakeComponent() : Component(ComponentType.Snake) {
     var nextPieceId: String? = null
+    var invisible: Boolean = false
 
     override fun updateFromJSON(data: JSONObject) {
         nextPieceId = data.getString("next_entityId")
         if(nextPieceId == "null")
             nextPieceId = null
+        invisible = data.getString("invisible").toBoolean()
     }
 }
