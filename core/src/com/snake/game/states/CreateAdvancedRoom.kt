af047986ce4ba61651f5f329a2b291973ea5abc5
vdonadze <vakho159@gmail.com>
2020-04-25 23:45:31 +0200

    password field

--- core/src/com/snake/game/states/CreateAdvancedRoom.kt
+++ core/src/com/snake/game/states/CreateAdvancedRoom.kt
@@ -13,6 +13,8 @@ class CreateAdvancedRoom : CreateRoom() {
             }
     private var passwordField = TextField("", skin, "big")
             .apply {
+                setPasswordCharacter('*')
+                isPasswordMode = true
                 messageText = "password"
                 setSize((ELEMENT_WIDTH - SPACING) * 3 / 4, ELEMENT_HEIGHT)
             }
