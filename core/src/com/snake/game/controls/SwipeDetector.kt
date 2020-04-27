51be6a82843993d3e6f3a217ebc1fe46b4782d17
Tom Pégeot <tom-pegeot@hotmail.fr>
2020-04-27 21:45:29 +0200

    add documentation

--- core/src/com/snake/game/controls/SwipeDetector.kt
+++ core/src/com/snake/game/controls/SwipeDetector.kt
@@ -5,6 +5,15 @@ import com.snake.game.backend.Data
 import com.snake.game.backend.Events
 import com.snake.game.backend.SocketService
 
+
+/*
+object that detect swipe
+And send the swipe event to the server if activated (input-system.ts)
+(based on the location of the mouse at the beginning and the and of the "mouse down" or touch)
+to implement it a multiplexer as to be used (because the game use also inputs in stage)
+multiplexer is possible because true is never send after input event therefore the next elements in
+the multiplexer will receive them as if the swipeDetector does not exist
+ */
 object SwipeDetector : GestureDetector(DirectionGestureListener()) {
     var active: Boolean = false
     public fun up() {
