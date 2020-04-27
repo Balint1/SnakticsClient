5830387ae505a6d10d783c280bfc411dea3ce358
KelianB <kelian.baert@gmail.com>
2020-04-27 16:07:32 +0200

    fixed rendering

--- core/src/com/snake/game/ecs/system/RenderingSystem.kt
+++ core/src/com/snake/game/ecs/system/RenderingSystem.kt
@@ -1,6 +1,9 @@
 package com.snake.game.ecs.system
 
+import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.graphics.Color
+import com.badlogic.gdx.graphics.GL20
+import com.badlogic.gdx.graphics.GL20.*
 import com.badlogic.gdx.graphics.g2d.BitmapFont
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer
@@ -90,20 +93,32 @@ object RenderingSystem : System(ComponentType.Position) {
             if (entity.hasComponent(ComponentType.Tag)) {
                 val tag = (entity.getComponent(ComponentType.Tag) as TagComponent).tag ?: return
 
-                // Render snake head
-                if (tag == TagComponent.EntityTagType.SnakeHead) {
-                    renderSnake(em, entity, sr)
-                } else if (tag == TagComponent.EntityTagType.SnakeBody) {
-                } else if (tag == TagComponent.EntityTagType.Fireball) {
-                    sr.begin(ShapeType.Filled)
-                    sr.color = Color.RED
-                    sr.rect(pos.x - 10, pos.y - 10, 20f, 20f)
-                    sr.end()
-                } else if (tag == TagComponent.EntityTagType.Food) {
-                    sr.begin(ShapeType.Filled)
-                    sr.color = Color.GREEN
-                    sr.rect(pos.x - 10, pos.y - 10, 20f, 20f)
-                    sr.end()
+                when (tag) {
+                    TagComponent.EntityTagType.SnakeHead -> {
+                        /*sr.begin(ShapeType.Filled)
+                        sr.color = Color.RED
+                        sr.rect(pos.x - 2, pos.y - 2, 4f, 4f)
+                        sr.end()*/
+                        renderSnake(em, entity, sr)
+                    }
+                    TagComponent.EntityTagType.SnakeBody -> {
+                        /*sr.begin(ShapeType.Filled)
+                        sr.color = Color.YELLOW
+                        sr.rect(pos.x - 2, pos.y - 2, 4f, 4f)
+                        sr.end()*/
+                    }
+                    TagComponent.EntityTagType.Fireball -> {
+                        sr.begin(ShapeType.Filled)
+                        sr.color = Color.RED
+                        sr.rect(pos.x - 10, pos.y - 10, 20f, 20f)
+                        sr.end()
+                    }
+                    TagComponent.EntityTagType.Food -> {
+                        sr.begin(ShapeType.Filled)
+                        sr.color = Color.GREEN
+                        sr.rect(pos.x - 10, pos.y - 10, 20f, 20f)
+                        sr.end()
+                    }
                 }
             } else {
                 // Temporary render
@@ -115,6 +130,13 @@ object RenderingSystem : System(ComponentType.Position) {
                 sr.end()
             }
         }
+
+        // Draw border around
+        var borderMargin = 3f
+        sr.begin(ShapeType.Line)
+        sr.color = Color.RED
+        sr.rect(borderMargin, borderMargin, worldWidth - 2*borderMargin, worldHeight - 2*borderMargin)
+        sr.end()
     }
 
     /**
@@ -143,6 +165,9 @@ object RenderingSystem : System(ComponentType.Position) {
      * @param snakeHead the first snake piece
      */
     private fun renderSnake(em: EntityManager, snakeHead: Entity, shapeRenderer: ShapeRenderer) {
+        Gdx.gl.glEnable(GL20.GL_BLEND);
+        Gdx.gl.glBlendFunc(GL20.GL_SRC_ALPHA, GL20.GL_ONE_MINUS_SRC_ALPHA);
+
         var playerComponent = snakeHead.getComponent(ComponentType.Player) as PlayerComponent
 
         // Stop rendering the snake once it is dead and decayed
@@ -153,23 +178,16 @@ object RenderingSystem : System(ComponentType.Position) {
         var headPosition = snakeHead.getComponent(ComponentType.Position) as PositionComponent
 
         val snakePoints: ArrayList<Vector2> = ArrayList()
-        // Add the head position twice to force the CatmullRomSpline to start there
-        snakePoints.add(Vector2(headPosition.x, headPosition.y))
         snakePoints.add(Vector2(headPosition.x, headPosition.y))
 
-        var pos: PositionComponent? = null
         while (pieceId != null) {
             if (!em.hasEntity(pieceId))
                 break
             var entity = em.getEntity(pieceId)!!
-            pos = entity.getComponent(ComponentType.Position) as PositionComponent
+            var pos = entity.getComponent(ComponentType.Position) as PositionComponent
             pieceId = (entity.getComponent(ComponentType.Snake) as SnakeComponent).nextPieceId
             snakePoints.add(Vector2(pos.x, pos.y))
         }
-
-        // Add the last position twice to force the CatmullRomSpline to end there
-        if (pos != null)
-            snakePoints.add(Vector2(pos.x, pos.y))
 
         var startColor = Color(0.3f, 0.75f, 0.8f, 1.0f)
         var endColor = Color(0.20f, 0.45f, 0.7f, 1.0f)
@@ -178,26 +196,24 @@ object RenderingSystem : System(ComponentType.Position) {
             startColor = Color(0.3f, 0.75f, 0.8f, 1.0f)
             endColor = Color(0.20f, 0.45f, 0.7f, 1.0f)
         }
-
-        if(playerComponent.color == "GreenSnake"){
+        else if(playerComponent.color == "GreenSnake"){
             startColor = Color(0.235f, 0.980f, 0f, 1.0f)
             endColor = Color(0.552f, 0.886f, 0.678f, 1.0f)
         }
 
-        // Handle blinding during decay
+        // Handle blinking during decay
         var alpha = 1.0f
         if (!playerComponent.alive && playerComponent.decaying) {
             val blinkTicks = 30f
-            var t = (RenderingConstants.SNAKE_DECAYING_TICKS - playerComponent.remainingDecayTicks) / blinkTicks.toFloat()
-            t = t.pow(1.25f) // makes bounces faster and faster
+            var t: Float = (RenderingConstants.SNAKE_DECAYING_TICKS - playerComponent.remainingDecayTicks) / blinkTicks
+            t = t.pow(1.25f) // blinks faster and faster
             alpha = 1f - max(0f, sin(2f*Math.PI * (t - 0.5f)).toFloat())
             startColor = Color(0.8f, 0.2f, 0.2f, alpha)
             endColor = Color(0.6f, 0.12f, 0.12f, alpha)
         }
 
-        // number of points used for rendering the spline
+        // Number of interpolated points between two snake body pieces
         var pointsDensity = 3
-        //var k = if (snakePoints.size > 2) snakePoints.size * pointsDensity else 1
 
         // Group points into array of close points (prevents issues when one side of the snake is on the other side of the board)
         var splines: ArrayList<CatmullRomSpline<Vector2>> = ArrayList()
@@ -208,22 +224,17 @@ object RenderingSystem : System(ComponentType.Position) {
             var pre = snakePoints[i-1]
             var p = snakePoints[i]
             if((p.x - pre.x).pow(2) + (p.y - pre.y).pow(2) > (RenderingConstants.SNAKE_CIRCLE_RADIUS * 4).pow(2)) {
+                currentSplinePoints.add(pre) // force the spline to go all the way to the last point
                 splines.add(CatmullRomSpline<Vector2>(currentSplinePoints.toTypedArray(), false))
                 currentSplinePoints.clear()
+                currentSplinePoints.add(p) // force the spline to start at that point
             }
             currentSplinePoints.add(p)
         }
         splines.add(CatmullRomSpline<Vector2>(currentSplinePoints.toTypedArray(), false))
 
+        // Use the splines to interpolate an a list of points
         var points = ArrayList<Vector2>()
-
-        /*val spline = CatmullRomSpline<Vector2>(snakePoints.toTypedArray(), false)
-        for (i in 0 until k) {
-            var pt = Vector2()
-            spline.valueAt(pt, i / (k - 1).toFloat())
-            points.add(pt)
-        }*/
-
         for(s in splines) {
             var k = if (s.controlPoints.size > 2) s.controlPoints.size * pointsDensity else 0
             if(s.controlPoints.size <= 2) {
@@ -259,30 +270,32 @@ object RenderingSystem : System(ComponentType.Position) {
             }
         }
 
-        shapeRenderer.begin(ShapeType.Filled)
-
         // Draw shadow
+        shapeRenderer.begin(ShapeType.Filled)
         for (i in 0 until points.size) {
             shapeRenderer.color.set(Color(0f, 0f, 0f, 0.1f*alphas[i]))
             shapeRenderer.circle(points[i].x, points[i].y - 2, RenderingConstants.SNAKE_CIRCLE_RADIUS)
         }
+        shapeRenderer.end()
 
         // Draw border around local snake
         if (playerComponent.playerId == SnakeECSEngine.localPlayerId) {
+            shapeRenderer.begin(ShapeType.Filled)
             shapeRenderer.color = Color(0.83f, 0.69f, 0.22f, 1f)
             for (i in 0 until points.size - 1) {
-                shapeRenderer.color.a = alphas[i]
-                shapeRenderer.circle(points[i].x, points[i].y, RenderingConstants.SNAKE_CIRCLE_RADIUS + 1f)
+                shapeRenderer.color.a = alphas[i] * 0.5f
+                shapeRenderer.circle(points[i].x, points[i].y, RenderingConstants.SNAKE_CIRCLE_RADIUS + 0.6f)
             }
+            shapeRenderer.end()
         }
 
         // Draw the snake
+        shapeRenderer.begin(ShapeType.Filled)
         for (i in 0 until points.size) {
             shapeRenderer.color.set(startColor).lerp(endColor, i / points.size.toFloat())
             shapeRenderer.color.a = alphas[i]
             shapeRenderer.circle(points[i].x, points[i].y, RenderingConstants.SNAKE_CIRCLE_RADIUS)
         }
-
         shapeRenderer.end()
     }
 }
